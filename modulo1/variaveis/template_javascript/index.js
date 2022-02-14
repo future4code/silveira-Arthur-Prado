02cf4a11ddd5bd1ef92c7507f3c943970026356e
arthurpraado <arthurbmprado@gmail.com>
2022-02-14 17:28:44 -0300

    Exercícios primeiro dia JavaScript

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,74 @@
+// Exercício 1 = 10 / 5
+
+// Exercício 2 = 10 / 10 / 10
+
+// Exercício 3 
+// p = horasTrabalhadasPorDia
+// t = pagamentoDiario
+
+let nome
+let idade
+
+console.log (typeof nome, typeof idade);
+
+
+// Exercício 1 d) Escritas de Código
+/* A resposta é undefined pois não acrescentamos um valor para as variáveis. */
+
+let qualSeuNome= prompt ("Qual seu nome?");
+let qualSuaIdade= prompt ("Qual a sua idade?");
+console.log (typeof qualSeuNome, typeof qualSuaIdade);
+
+// Exercicio 1 f) 
+// As duas novas variáveis deram String, pois ainda não fiz a conversão de tipos para alterar a idade de texto para número.
+
+console.log ("Olá", qualSeuNome,", você tem", qualSuaIdade, "anos.");
+
+const qualSeuCalçado= prompt ("Você está de chinelo?");
+const estaChovendo= prompt ("Hoje está chovendo?");
+const gostaDeDancar= prompt ("Você gosta de dançar?")
+console.log ("Você está de chinelo?", qualSeuCalçado);
+console.log ("Hoje está chovendo?", estaChovendo);
+console.log ("Você Gosta De Dançar?", gostaDeDancar);
+
+let a = 10;
+let b = 25;
+let c = 10;
+
+// Aqui faremos uma lógica para trocar os valores
+
+a = b;
+b = c;
+c = a;
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a); 
+console.log("O novo valor de b é", b); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
