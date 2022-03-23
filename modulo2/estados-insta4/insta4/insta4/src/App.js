7f7058f90dee98584311e84eba8812e8d7595c92
arthurpraado <arthurbmprado@gmail.com>
2022-03-23 18:47:02 -0300

    Projeto Instagram

---
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />  
+        
+        <Post
+          nomeUsuario={'José'}
+          fotoUsuario={'https://picsum.photos/50/50?b'}
+          fotoPost={'https://picsum.photos/200/150?a'}
+        />
+
+        <Post
+          nomeUsuario={'João'}
+          fotoUsuario={'https://picsum.photos/50/50?c'}
+          fotoPost={'https://picsum.photos/200/150?d'}
+        />
+
+      </MainContainer>
+
+     
+
+      
+    );
+  }
+}
+
+export default App;
