7949af69cbe9b7d667f5f48964a3d2ec274fe0c0
Arthur Branco Martins Prado <arthurbmprado@gmail.com>
2022-07-27 18:14:29 -0300

    case 2 quase pronto

--- modulo6/projeto-brainn-case2/projeto-brainn/src/Components/SelectLoterias.js
+++ modulo6/projeto-brainn-case2/projeto-brainn/src/Components/SelectLoterias.js
@@ -1,119 +1,95 @@
-import { BaseURL } from "../Constants/BaseURL";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import DiaDeSorte from "../Pages/DiaDeSorte";
-import LotoFacil from "../Pages/LotoFacil";
-import LotoMania from "../Pages/LotoMania";
-import MegaSena from "../Pages/MegaSena";
-import Quina from "../Pages/Quina";
-import TimeMania from "../Pages/TimeMania";
+import React from "react";
+import styled from "styled-components";
 
-const SelectLoterias = () => {
-  const [loterias, setLoterias] = useState([]);
-  const [controleInput, setControleInput] = useState(0);
-  const [loteriasConcursos, setLoteriasConcursos] = useState([]);
-  const [concurso, setConcurso] = useState({});
+const loteriaCores = {
+  0: "#6BEFA3",
+  1: "#8666EF",
+  2: "#DD7AC6",
+  3: "#FFAB64",
+  4: "#5AAD7D",
+  5: "#BFAF83",
+};
 
-  const renderizaPagina = () => {
-    switch (controleInput) {
-      case 1:
-        return <Quina {...concurso} />;
-      case 2:
-        return <LotoFacil {...concurso} />;
-      case 3:
-        return <LotoMania {...concurso} />;
-      case 4:
-        return <TimeMania {...concurso} />;
-      case 5:
-        return <DiaDeSorte {...concurso} />;
-      default:
-        return <MegaSena {...concurso} />;
-    }
-  };
 
-  const pegaLoterias = () => {
-    const url = `${BaseURL}/loterias`;
-    axios
-      .get(url)
-      .then((response) => {
-        setLoterias(response.data);
-      })
-      .catch((error) => {
-        console.log(error.data);
-      });
-  };
 
-  const pegaConcursos = () => {
-    const url = `${BaseURL}/loterias-concursos`;
-    axios
-      .get(url)
-      .then((response) => {
-        setLoteriasConcursos(response.data);
-      })
-      .catch((error) => {
-        console.log(error.data);
-      });
-  };
+const Titulo = styled.h3`
+  text-transform: uppercase;
+  color: white;
+  margin: 50px;
 
-  const pegaConcursoPorId = () => {
-    // controleInput é 0 == estou na Mega Sena
-    // preciso buscar no array de concursos usando o valor do controle input que é o tipo de sorteio que eu selecionei.
-    const { concursoId } = loteriasConcursos?.find((lc) => {
-      return lc.loteriaId === controleInput;
-    });
-    const url = `${BaseURL}/concursos/${concursoId}`;
-    axios
-      .get(url)
-      .then((response) => {
-        setConcurso(response.data);
-      })
-      .catch((error) => {
-        console.log(error.data);
-      });
-  };
+`;
 
-  useEffect(() => {
-    pegaLoterias();
-    pegaConcursos();
-  }, []);
+const Background = styled.div`
+  background-color: ${(props) => loteriaCores[props.id]};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
  
-  useEffect(() => {
-   if(controleInput > -1 && loteriasConcursos.length) {
-    pegaConcursoPorId();
-   }
-  }, [loteriasConcursos, controleInput]);
+`;
 
-  const onChangeSelect = (event) => {
-    setControleInput(Number(event.target.value));
-  };
+const DivNumeros = styled.div`
+  background-color: #efefef;
+  flex: 1;
+  display: table;
+  table-layout: fixed;
+  padding-left: 25px;
+`;
 
-  // const renderizaLoterias = loterias.map((item) => {
-  //   return <option value={item.id}>{item.nome}</option>;
-  // });
+const Radius = styled.div`
+  background-color: #efefef;
+  color: #efefef;
+  border-radius: 95% 95% 0 0;
+  margin-top: -100px;
+`;
+
+const Main = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NumeroConcursos = styled.h6`
+  color: white;
+  margin: 120px;
+`
+
+const NumerosStyled = styled.ul`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background-color: white;
+  text-align: center;
+  align-items: center;
+  margin: 15px;
+  display: table-cell;
+  overflow: hidden;
+  float: left;
+  padding: 15px;
+`
+
+const SelectLoterias = ({ data, id, loteria, numeros, loterias, children }) => {
+  const sorteiaNumeros = numeros.map((numero) => {
+    return <NumerosStyled key={numero}> {numero} </NumerosStyled>;
+  });
+
+  const loteriaAtual = loterias.find((l) => {
+    return l.id === loteria;
+  });
 
   return (
-    <div>
-      {/* <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={renderizaLoterias}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label={"Loterias"} />}
-
-      /> */}
-      <div>{renderizaPagina()}</div>
-      <div>
-        <select value={controleInput} onChange={onChangeSelect}>
-          {loterias?.map((loteria, index) => {
-            return (
-              <option key={index} value={loteria.id}>
-                {loteria.nome}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-    </div>
+    <Main>
+      <Background id={loteria}>
+        {children}
+        <Titulo>{loteriaAtual?.nome}</Titulo>
+        <NumeroConcursos>Concurso Nº{id}</NumeroConcursos>
+      </Background>
+      <Radius>
+        <p>a</p>
+      </Radius>
+      <DivNumeros>
+        <ul><b>{sorteiaNumeros}</b></ul>
+      </DivNumeros>
+    </Main>
   );
 };
 
